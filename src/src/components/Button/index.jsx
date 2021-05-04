d83f465676a8cbe4d8a71a4ab8b9be7c5582cb12
Nurio Fernández <personal@nurio.me>
2021-05-04 11:12:16 +0200

    Added Button component

---
+++ src/src/components/Button/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import "./style.css"
+
+class Button extends Component {
+
+    render() {
+        return (
+            <button className="Button">
+                {this.props.children}
+            </button>
+        );
+    }
+
+}
+
+export default Button;
