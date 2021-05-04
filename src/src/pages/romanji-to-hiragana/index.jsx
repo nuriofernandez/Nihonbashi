aa171867e71a7050d1760f8609a5a6789e3c87a8
Nurio Fernández <personal@nurio.me>
2021-05-04 12:22:58 +0200

    Added RomanjiToHiragana page

---
+++ src/src/pages/romanji-to-hiragana/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import QuestionDialog from '../../views/QuestionDialog'
+
+class RomanjiToHiragana extends Component {
+
+    render() {
+        return (
+            <QuestionDialog />
+        );
+    }
+
+}
+
+export default RomanjiToHiragana;
