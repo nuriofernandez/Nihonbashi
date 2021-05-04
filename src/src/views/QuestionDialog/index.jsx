55c08b608ac0d361e5b398d37c92d76de8feb790
Nurio Fernández <personal@nurio.me>
2021-05-04 14:12:19 +0200

    Implemented state to QuestionDialog

--- src/src/views/QuestionDialog/index.jsx
+++ src/src/views/QuestionDialog/index.jsx
@@ -8,18 +8,53 @@ import "./style.css"
 
 class QuestionDialog extends Component {
 
+    state = {
+        correct: "...",
+        answers: ["...", "...", "...", "..."]
+    };
+
+    constructor(props) {
+        super(props);
+
+        // TODO use something like a map for this.
+        this.state = {
+            correct: "つ",
+            answers: ["tsu", "ku", "shi", "go"]
+        }
+    }
+
+    handleAnswer(answerId) {
+        const answer = this.state.answers[answerId];
+        const { correct } = this.state;
+
+        if (answer !== correct) {
+            this.handleFail();
+            return;
+        }
+
+        this.handleCorrect();
+    }
+
+    handleFail() {
+        alert("NO!");
+    }
+
+    handleCorrect() {
+        alert("YESHHH");
+    }
+
     render() {
         return (
             <BoxArea>
                 <div className="QuestionDialog_area">
                     <div className="QuestionDialog_preview">
-                        <Preview>つ</Preview>
+                        <Preview>{this.state.correct}</Preview>
                     </div>
                     <div className="QuestionDialog_answers">
-                        <Button>tsu</Button>
-                        <Button>ku</Button>
-                        <Button>shi</Button>
-                        <Button>go</Button>
+                        <Button onClick={() => this.handleAnswer(0)}>{this.state.answers[0]}</Button>
+                        <Button onClick={() => this.handleAnswer(1)}>{this.state.answers[1]}</Button>
+                        <Button onClick={() => this.handleAnswer(2)}>{this.state.answers[2]}</Button>
+                        <Button onClick={() => this.handleAnswer(3)}>{this.state.answers[3]}</Button>
                     </div>
                 </div>
             </BoxArea>
