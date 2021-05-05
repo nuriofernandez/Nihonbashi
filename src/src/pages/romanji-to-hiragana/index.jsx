41ead38481649a9ea7e7eb8757aa2c8a241ef3fc
Nurio Fernández <personal@nurio.me>
2021-05-05 22:36:56 +0200

    Implemented random generation of questions

--- src/src/pages/romanji-to-hiragana/index.jsx
+++ src/src/pages/romanji-to-hiragana/index.jsx
@@ -1,25 +1,37 @@
 import React, { Component } from 'react'
 
+import Maths from '../../utils/Maths'
+
 import { characters } from './characters-database'
 import QuestionDialog from '../../views/QuestionDialog'
 
 class RomanjiToHiragana extends Component {
 
     state = {
-        correct: "...",
-        answers: ["...", "...", "...", "..."]
+        correct: characters[0],
+        answers: [
+            characters[0],
+            characters[1],
+            characters[2],
+            characters[3]
+        ]
     };
 
     handleAnswer(answerId) {
+        const correct = characters[Maths.randomNumberBetween(0, 20)];
         this.setState({
-            correct: characters[0].hiragana,
-            answers: [
-                characters[0].romanji,
-                characters[1].romanji,
-                characters[2].romanji,
-                characters[3].romanji
-            ]
+            correct: correct,
+            answers: this.generateAnswers(correct)
         })
+    }
+
+    generateAnswers(correctAnswer) {
+        return [
+            correctAnswer,
+            characters[Maths.randomNumberBetween(0, 20)],
+            characters[Maths.randomNumberBetween(0, 20)],
+            characters[Maths.randomNumberBetween(0, 20)]
+        ]
     }
 
     handleFail() {
@@ -32,11 +44,17 @@ class RomanjiToHiragana extends Component {
 
     render() {
         const { correct, answers } = this.state;
+        const romanjiAnsewers = [
+            answers[0].romanji,
+            answers[1].romanji,
+            answers[2].romanji,
+            answers[3].romanji
+        ]
         return (
             <QuestionDialog
                 handleAnswer={(answerId) => this.handleAnswer(answerId)}
-                correct={correct}
-                answers={answers}
+                correct={correct.hiragana}
+                answers={romanjiAnsewers}
             />
         );
     }
